e25f3c872b4217554b9e404205a4cf04973fb309
Shimon <shimonbhuiya02@gmail.com>
2025-07-22 19:49:20 +0600

    start new Branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
-  runApp();
+  runApp(myApp());
+}
+class myApp extends StatelessWidget{
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeActivity());
+  }
+
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("HomeActivity"),),
+
+      body: Center(
+        
+        child: ElevatedButton(onPressed: (){
+          print("Button Clicked");
+        }, child:Text("Login")),
+        
+      ),
+    );
+  }
+
 }
